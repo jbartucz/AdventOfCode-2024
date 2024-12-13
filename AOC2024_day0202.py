8f06514d43d2161f4077bd9c3400d5801ddb38ae
jbartucz <john@bartucz.com>
2024-12-13 13:12:58 -0600

    day 2 finished

--- AOC2024_day0202.py
+++ AOC2024_day0202.py
@@ -1,62 +1,37 @@
 
 def checkvalid(nums):
 
-    dir = 0
-    if nums == sorted(nums):
-        dir = 1
-    elif nums == sorted(nums, reverse=True):
-        dir = -1
-    else:
+    if nums != sorted(nums) and nums != sorted(nums, reverse=True):
         return False
 
     for i in range(len(nums) - 1):
         diff = abs(nums[i+1] - nums[i])
         if diff > 3 or diff == 0:
             return False
-            
 
+    return True
 
 totalsum = 0
 
 with open("AOC2024_day02.txt") as inpt:
     badlist = []
     for line in inpt:
-        numss = line.strip().split()
-        nums = []
-        for n in numss:
-            nums.append(int(n))
-        print(nums)
+        nums = [int(x) for x in line.strip().split()]
+        #print(nums)
 
-        dir = 0
-        if nums == sorted(nums):
-            dir = 1
-        elif nums == sorted(nums, reverse=True):
-            dir = -1
-        else:
-            badlist.append(nums)
-            continue # not in order
-
-        numbad = 0
-        allbad = False
-        for i in range(len(nums) - 1):
-            diff = abs(nums[i+1] - nums[i])
-            if diff > 3 or diff == 0:
-                if numbad > 0:
-                    allbad = True
-                    break
-                else:
-                    numbad += 1
-                    testnums = list(nums)
-                    testnums.pop(i)
-                    if checkvalid(testnums) == False:
-                        allbad = True
-                        break
-        
-        if allbad == False and numbad > 0:
-            print(f"* {nums}")
-
-        if allbad == False:
+        if checkvalid(nums) == True:
             totalsum += 1
+            #print(f"YES {nums}")
+        else:
+            #print(f"NO  {nums}")
+            for i in range(len(nums)):
+                newnums = list(nums)
+                newnums.pop(i)
+                #print(f"TRYING {newnums}")
+                if checkvalid(newnums) == True:
+                    # print(f"---YES {newnums}")
+                    totalsum += 1
+                    break
 
 
 print(totalsum)
