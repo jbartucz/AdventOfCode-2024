01c9bef09e490f3c128d4214385e2dac2eabb764
jbartucz <john@bartucz.com>
2024-12-23 10:01:14 -0600

    ?

---
+++ AOC2024_day2202.py
@@ -0,0 +1,30 @@
+
+def doit(num):
+
+    
+    for i in range(2000):
+        res = num * 64
+        num = num ^ res
+        num = num % 16777216
+
+        res = num // 32
+        num = num ^ res
+        num = num % 16777216
+
+        res = num * 2048
+        num = num ^ res
+        num = num % 16777216
+
+        # print(num)
+
+    return num
+
+
+totalsum = 0
+with open("AOC2024_day22.txt") as finpt:
+    for inpt in finpt:
+        current = int(inpt)
+        totalsum += doit(current)
+
+print(totalsum)
+
